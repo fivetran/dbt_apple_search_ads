7debb9eb29e99066f7cc0e2b16550067f2c4fbee
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2022-01-24 11:07:08 -0600

    review comments

--- models/apple_search_ads__ad_group_report.sql
+++ models/apple_search_ads__ad_group_report.sql
@@ -1,15 +1,20 @@
+-- See below for requested styling updates
 with ad_group_report as (
     select *
     from {{ var('ad_group_report') }}
+
 ), ad_group as (
     select * 
     from {{ ref('int_apple_search_ads__most_recent_ad_group') }}
+
 ), campaign as (
     select *
     from {{ ref('int_apple_search_ads__most_recent_campaign') }}
+
 ), organization as (
     select * 
     from {{ var('organization') }}
+
 ), joined as (
     select 
         organization.organization_id,
@@ -18,7 +23,7 @@ with ad_group_report as (
         campaign.campaign_name, 
         ad_group.ad_group_id,
         ad_group.ad_group_name,
-        ad_group_report.date_day,
+        ad_group_report.date_day, --
         ad_group_report.currency,
         ad_group_report.taps,
         ad_group_report.new_downloads,
@@ -27,9 +32,13 @@ with ad_group_report as (
         ad_group_report.impressions,
         ad_group_report.spend
     from ad_group_report
-    join ad_group on ad_group_report.ad_group_id = ad_group.ad_group_id
-    join campaign on ad_group.campaign_id = campaign.campaign_id
-    join organization on ad_group.organization_id = organization.organization_id
+    join ad_group 
+        on ad_group_report.ad_group_id = ad_group.ad_group_id
+    join campaign 
+        on ad_group.campaign_id = campaign.campaign_id
+    join organization 
+        on ad_group.organization_id = organization.organization_id
 )
 
-select * from joined
+select * 
+from joined
